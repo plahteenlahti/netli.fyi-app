08758159a6cacbcb7cc2c2bf05f1ea8194db2205
Perttu Lähteenlahti <perttu.lahteenlahti@gmail.com>
2021-03-31 14:35:55 +0300

    Test if this works

--- src/typings/react-native-ultimate-config'.ts
+++ src/typings/react-native-ultimate-config'.ts
@@ -1,7 +1,10 @@
+declare module 'react-native-ultimate-config'
+
 export interface ConfigVariables {
   redirect_url: string
   client_secret: string
   client_id: string
+  client: string
 }
 
 declare const UltimateConfig: ConfigVariables
