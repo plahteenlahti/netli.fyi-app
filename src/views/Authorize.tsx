eb89b598516e389c3ab81dac7ca8d2523ea6dc3f
Perttu Lähteenlahti <perttu@lahteenlahti.com>
2022-08-21 13:19:06 +0300

    chore(meta): fix app naming

--- src/views/Authorize.tsx
+++ src/views/Authorize.tsx
@@ -131,8 +131,9 @@ export const Authorize: FC<Props> = ({ navigation }) => {
         <ORSection>
           <InputContainer>
             <Input
-              onChangeText={(text) => setPersonalAccessToken(text)}
+              onChangeText={text => setPersonalAccessToken(text)}
               placeholder="Personal access token"
+              value="wTB3vBHU5t7OQNU7-nOKa6mMTVABTt_fhMwwMtL7Cjk"
             />
             <AuthorizeButtonSmall onPress={authenticateWithToken}>
               <ButtonText>Authorize</ButtonText>
