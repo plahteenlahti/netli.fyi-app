1b76a887d9b0dfc22a1763029fe23bf80ad70844
Perttu Lähteenlahti <perttu@lahteenlahti.com>
2022-10-08 08:55:47 +0300

    fix(tests): fix tests and ci

--- __tests__/App.owl.tsx
+++ __tests__/App.owl.tsx
@@ -1,5 +1,7 @@
 import { takeScreenshot } from 'react-native-owl'
 
+jest.setTimeout(3000)
+
 describe('App.tsx', () => {
   it('takes a screenshot of the first screen', async () => {
     const screen = await takeScreenshot('Login')
