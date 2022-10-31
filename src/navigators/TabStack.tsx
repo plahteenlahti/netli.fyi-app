7747ae46c73b488f22fe56c86cfab552fcf6b26c
Perttu Lähteenlahti <perttu@lahteenlahti.com>
2022-10-31 23:24:15 +0200

    fix(tab bar): fix icons

--- src/navigators/TabStack.tsx
+++ src/navigators/TabStack.tsx
@@ -2,18 +2,38 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { SitesStack } from './SitesStack'
 import { BuildsStack } from './BuildsStack'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import { useTheme } from 'styled-components/native'
 
 const Tab = createBottomTabNavigator()
 
 export const TabStack = () => {
+  const theme = useTheme()
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: theme.accentColor,
+        tabBarInactiveTintColor: theme.secondaryTextColor
+      }}>
       <Tab.Screen
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="list" color={color} size={15} />
+          )
+        }}
         name="Sites"
         component={SitesStack}
       />
-      <Tab.Screen name="Builds" component={BuildsStack} />
+      <Tab.Screen
+        name="Builds"
+        component={BuildsStack}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="hammer" color={color} size={15} />
+          )
+        }}
+      />
     </Tab.Navigator>
   )
 }
