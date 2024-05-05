f7f9f70af0ee33babadc48b0e4b04e3ab78e4753
Perttu Lähteenlahti <perttu@noice.com>
2024-05-05 12:00:00 +0300

    Refactor: import aliases

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,33 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['nativewind/babel', 'react-native-reanimated/plugin']
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx'
+        ],
+        alias: {
+          '@components': './src/components',
+          '@utilities': './src/utilities',
+          '@views': './src/views',
+          '@assets': './src/assets',
+          '@store': './src/store',
+          '@navigators': './src/navigators',
+          '@styles': './src/styles',
+          '@hooks': './src/hooks',
+          '@typings': './src/typings'
+        }
+      }
+    ],
+    'nativewind/babel',
+    'react-native-reanimated/plugin'
+  ]
 }
